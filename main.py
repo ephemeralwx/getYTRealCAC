29b77d0514851b7167a902b1f26b5ed46c037879
Kevin <kevinx8017@gmail.com>
2023-10-23 13:23:27 -0400

    changedit

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 openai.api_key = OPENAI_API_KEY  # Set the API key
 YOUTUBE_API_KEY = 'AIzaSyBIB84RbnY_3RkmEgTlilt--dx4_MPW40A'
 
+
 @app.route('/get_transcript', methods=['GET'])
 def get_transcript():
     youtube_url = request.args.get('youtube_url')
@@ -104,11 +105,9 @@ def call_openai_api(prompt, max_tokens):
 def get_top_3_videos(api_key, query, recency_year, n=3):
     youtube = build('youtube', 'v3', developerKey=api_key)
 
-    # Get the current year and calculate the cutoff publication year
     current_year = datetime.now().year
     cutoff_year = current_year - recency_year
 
-    # Search for videos based on the query and sort by view count
     search_response = youtube.search().list(
         q=query,
         type="video",
@@ -121,7 +120,6 @@ def get_top_3_videos(api_key, query, recency_year, n=3):
     for item in search_response['items']:
         video_id = item['id']['videoId']
 
-        # Fetch video content details and statistics
         video_response = youtube.videos().list(
             id=video_id,
             part="contentDetails,statistics,snippet"
@@ -139,62 +137,36 @@ def get_top_3_videos(api_key, query, recency_year, n=3):
                 views = int(video['statistics']['viewCount'])
                 ratio = likes / (likes + dislikes) if (likes + dislikes) > 0 else 0
                 
-                # The scoring mechanism amplifies the effect of the like/dislike ratio using an exponent.
-                # This way, videos with strong positive reception (high like/dislike ratios) get more advantage.
-                # Example:
-                # Video A: 20 million views, 1:1 like/dislike -> score = 10 million
-                # Video B: 10 million views, 19:1 like/dislike -> score > 10 million (even with half the views of A)
-                # This allows us to prioritize audience reception while still considering popularity.
                 score = views * (ratio ** n)
 
                 video_candidates.append({
                     'score': score,
                     'title': video['snippet']['title'],
-                    'link': f"https://www.youtube.com/watch?v={video_id}"
+                    'link': f"https://www.youtube.com/watch?v={video_id}",
+                    'published_at': video['snippet']['publishedAt'],
+                    'channel_title': video['snippet']['channelTitle'],
+                    'view_count': views,
+                    'like_count': likes,
+                    'dislike_count': dislikes
                 })
 
-    # Sort by score and select the top 3
     top_videos = sorted(video_candidates, key=lambda x: x['score'], reverse=True)[:3]
-
-    return [(video['title'], video['link']) for video in top_videos]
+    return top_videos
 @app.route('/youtube_summary', methods=['POST'])
 def youtube_summary():
     data = request.get_json()
     prompt = data.get('prompt')
-    recency = data.get('recency', 2)  # default to 2 years if recency is not provided
+    recency = data.get('recency', 2)
 
     if not prompt:
         return jsonify({'error': 'Prompt parameter is required'}), 400
 
     try:
         videos = get_top_3_videos(YOUTUBE_API_KEY, prompt, recency)
-        summaries = {}
-
-        for i, (title, link) in enumerate(videos):
-            video_id = link.split("v=")[-1]
-            transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
-            transcript_text = " ".join([entry['text'] for entry in transcript_list])
-            openai_response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {
-                        "role": "user",
-                        "content": f"Summarize the following content in a concise manner: {transcript_text}\n\nKeep your response under 1000 characters."
-                    }
-                ],
-                max_tokens=200  # Adjust as necessary to fit within your character limit
-            )
-            summary = openai_response['choices'][0]['message']['content'].strip()
-            summaries[f'video{i + 1}'] = {
-                'title': title,
-                'link': link,
-                'summary': summary
-            }
-
-        return jsonify({'videos': summaries})
-
+        return jsonify({'videos': videos})
     except Exception as e:
         return jsonify({'error': f'Unexpected error: {e}'}), 500
+
 
 
 
